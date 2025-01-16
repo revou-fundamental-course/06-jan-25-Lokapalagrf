f633cc19afd2abe1f639dd868426ee6ecdb4c27b
lokapalagrf <lokapalagrf@gmail.com>
2025-01-16 13:21:28 +0700

    perubahan kedua

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,32 @@
 // Ini File Javascript
+
+
+// Slide Gambar Banner
+let indexBanner = 0;
+
+changeBackground();
+
+function nextBanner() {
+    indexBanner = indexBanner + 1;
+
+    changeBackground();
+}
+
+function changeBackground() {
+    let bannerList = document.getElementsByClassName('banner-image');
+    
+    if (indexBanner > bannerList.length; i++){
+        // Reset indexBanner
+        indexBanner = 0;
+    }
+
+    // Looping to change bckground
+    for (let i = 0; i < bannerList.length; i++){
+        bannerList[i].style.display = 'none';
+    }
+
+    bannerList[indexBanner].Style.display = 'block';
+}
+
+setInterval(nextBanner, 3000);
+ 
